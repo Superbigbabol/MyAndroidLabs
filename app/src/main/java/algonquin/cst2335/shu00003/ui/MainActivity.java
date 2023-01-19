9a69814f2d3b0c2cc376fe244655c698b042511f
Bo Shu <shu00003@algonquinlive.com>
2023-01-18 21:34:28 -0500

    Finished week2

--- app/src/main/java/algonquin/cst2335/shu00003/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/shu00003/ui/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import algonquin.cst2335.shu00003.data.MainViewModel;
 import algonquin.cst2335.shu00003.databinding.ActivityMainBinding;
@@ -36,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
 //        EditText myedit = findViewById(R.id.myedittext);
         EditText myedit = variableBinding.myedittext;
 
+        CheckBox mycheckbox = variableBinding.mycheckbox;
+        Switch myswitch = variableBinding.myswitch;
+        RadioButton myradiobutton = variableBinding.myradiobutton;
+
+        ImageView myimage = variableBinding.myimage;
+        ImageButton myimagebtn = variableBinding.myimagebtn;
+
 //        btn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -49,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
         btn.setOnClickListener( vw -> {
             model.editString.postValue(myedit.getText().toString());
-
         });
 
 //        model.editString.observe(this, new Observer<String>() {
@@ -58,5 +72,38 @@ public class MainActivity extends AppCompatActivity {
 //                mytext.setText("Your edit text has:"+ s);
 //            }
 //        });
+
+        model.isSelected.observe(this,
+                selected -> {
+                    mycheckbox.setChecked(selected);
+                    myswitch.setChecked(selected);
+                    myradiobutton.setChecked(selected);
+                    Context context = getApplicationContext();
+                    CharSequence text = "The value is now:"+selected;
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context,text,duration);
+                    toast.show();
+                }
+        );
+
+        mycheckbox.setOnCheckedChangeListener((cb, isChecked) -> {
+            model.isSelected.postValue(mycheckbox.isChecked());
+        });
+
+        myswitch.setOnCheckedChangeListener((cb, isChecked) -> {
+            model.isSelected.postValue(myswitch.isChecked());
+        });
+
+        myradiobutton.setOnCheckedChangeListener((cb, isChecked) -> {
+            model.isSelected.postValue(myradiobutton.isChecked());
+        });
+
+        myimagebtn.setOnClickListener( vw -> {
+            Context context = getApplicationContext();
+            CharSequence text = "The width ="+myimagebtn.getWidth()+" and height="+myimagebtn.getHeight();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context,text,duration);
+            toast.show();
+        });
     }
 }
